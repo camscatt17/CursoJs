970b81c2aa74bac8d47882c7aaa96ff345dc1431
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-07-28 18:25:18 -0300

    Exercício da Aula 26 completo.

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -9,52 +9,61 @@ function meuEscopo() {
         const peso = Number(inputPeso.value);
         const altura = Number(inputAltura.value);
 
-        validacaoInput(peso, altura);
+        const validacao = validacaoInput(peso, altura);
 
-        const imc = calculoIMC(peso, altura);
-        const NivelIMC = getNivelIMC(imc);
+        if (validacao) {
+            const imc = calculoIMC(peso, altura);
+            const NivelIMC = getNivelIMC(imc);
 
-        const msg = `Seu IMC é ${imc} (${NivelIMC}).`;
-        setResultado(msg, true);
+            const msg = `Seu IMC é ${imc} (${NivelIMC}).`;
+            setResultado(msg, true);
+        } 
 
     });
 
-    function getNivelIMC(imc){
+    function getNivelIMC(imc) {
         const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
-        
-        if(imc >= 39.9) return nivel[5];
-        if(imc >= 34.9) return nivel[4];
-        if(imc >= 29.9) return nivel[3];
-        if(imc >= 24.9) return nivel[2];
-        if(imc >= 18.5) return nivel[1];
-        if(imc < 18.5)  return nivel[0];
+
+        if (imc >= 39.9) return nivel[5];
+        if (imc >= 34.9) return nivel[4];
+        if (imc >= 29.9) return nivel[3];
+        if (imc >= 24.9) return nivel[2];
+        if (imc >= 18.5) return nivel[1];
+        if (imc < 18.5) return nivel[0];
     };
 
-    function calculoIMC(peso, altura){
-        const imc = peso/altura**2;
+    function calculoIMC(peso, altura) {
+        const imc = peso / altura ** 2;
         return imc.toFixed(2);
     };
 
-    function validacaoInput(peso, altura){
-        if(!peso){
+    function validacaoInput(peso, altura) {
+        if (!peso) {
             setResultado('Peso inválido', false);
-            return;
-        }else if (!altura){
+            return 0;
+        } else if (!altura) {
             setResultado('Altura inválida', false);
-            return;
+            return 0;
         }
-        console.log('Cheguei aqui!');
+        return 1;
     };
 
-    function setResultado(msg, isValid){
+    function setResultado(msg, isValid) {
         const resultado = document.querySelector('.resultado');
         resultado.innerHTML = '';
         const p = criaParagrafo();
+
+        if (isValid) {
+            p.classList.add('resultado-paragrafo');
+        } else {
+            p.classList.add('bad');
+        }
+
         p.innerHTML = msg;
-        resultado.appendChild(p);      
+        resultado.appendChild(p);
     };
 
-    function criaParagrafo(){
+    function criaParagrafo() {
         const p = document.createElement('p');
         return p;
     };
